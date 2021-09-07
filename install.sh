9810b819861e85417ffd48fc3e39c4cd3179682c
chauvenne <remi.chv@gmail.com>
2021-09-07 11:50:12 +0200

    Install PowerLines fonts
    Signed-off-by: chauvenne <remi.chv@gmail.com>

--- install.sh
+++ install.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
+# Copy the config files in the home directory
+cp dotfiles/.* $HOME 2> /dev/null
+
 # Autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 # Syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/zsh-syntax-highlighting
 
-# Copy the config files in the home directory
-cp dotfiles/.* $HOME 2> /dev/null
+# Install powerline fonts
+git clone https://github.com/powerline/fonts.git $HOME/fonts
+cd $HOME/fonts
+./install.sh
